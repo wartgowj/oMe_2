893d6e566b461f691760eb81647d33f1ca330b92
wartgowj <wartgowj@gmail.com>
2018-03-05 17:30:30 -0800

    added view-controller

--- server.js
+++ server.js
@@ -36,10 +36,12 @@ app.set("view engine", "handlebars");
 const itemController = require("./controllers/item-controller.js");
 const userController = require("./controllers/user-controller.js");
 const transController = require("./controllers/transaction-controller.js");
+const viewController = require("./controllers/view-controller.js");
 
 app.use(itemController);
 app.use(userController);
 app.use(transController);
+app.use(viewController);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
