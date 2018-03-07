4b2f70cf2ef48bdec5a9c2f949ac315c2beeaab3
Joshua Cook <jcook@lynxsolutions.com>
2018-03-06 20:01:24 -0800

    adding modals and post logic

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -1,3 +1,70 @@
 $(document).ready(function() {
- 
+    //Click events
+    $(document).on("click", "button.addBorrow", handleAddBorrow);
+    $(document).on("click", "button.addLend", handleAddLend);
+    $(document).on("click", "button.borrow", handleAddBorrow);
+    $(document).on("click", "button.lend", handleAddLend);
+
+
+    $('.borrow').modal('open');
+
+
+
+
+
+    function handleAddBorrow(event) {
+        event.preventDefault();
+        // Wont submit the post if we are missing a body, title, or author
+        if (!item.val().trim() || !borrower.val().trim() || !dateDue.val()) {
+            return;
+        }
+        // Constructing a newPost object to hand to the database
+        var newBorrow = {
+            item_id: item
+                .val()
+                .trim(),
+            owner_id: owner
+                .val()
+                .trim(),
+            borrower_id: borrower.val(),
+            due_date: dueDate.val()
+        };
+            submitBorrow(newBorrow);
+
+    }
+
+    // Submits a new post and brings user to blog page upon completion
+    function submitBorrow(borrow) {
+        $.post("/:user/addBorrow", post, function() {
+            window.location.href = "/:user";
+        });
+    }
+    function handleAddLend(event) {
+        event.preventDefault();
+        // Wont submit the post if we are missing a body, title, or author
+        if (!item.val().trim() || !owner.val().trim() || !dateDue.val()) {
+            return;
+        }
+        // Constructing a newPost object to hand to the database
+        var newLend = {
+            item_id: item
+                .val()
+                .trim(),
+            owner_id: owner
+                .val()
+                .trim(),
+            borrower_id: borrower.val(),
+            due_date: dueDate.val()
+        };
+        submitLend(newLend);
+
+    }
+
+    // Submits a new post and brings user to blog page upon completion
+    function submitLend(Lend) {
+        $.post("/:user/addBorrow", post, function() {
+            window.location.href = "/:user";
+        });
+    }
+
 });
