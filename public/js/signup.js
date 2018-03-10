630398ad0c9208c827b9bdfbfd417ce3907f28e3
wartgowj <wartgowj@gmail.com>
2018-03-09 17:46:51 -0800

    username verification for new users

--- public/js/signup.js
+++ public/js/signup.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 
     function handleNewUser(event){
         event.preventDefault();
+        checkIfNameTaken(username);
         //checks that username was entered
         if (!username.val().trim()) {
             swal("Oops!", 'Please enter a Username!', 'error');
@@ -28,11 +29,26 @@ $(document).ready(function () {
             swal("Oops!", 'Please enter a mugshot URL!', 'error');
             return;
         }
-        insertUser({
-            name: username.val().trim(),
-            password: password.val().trim(),
-            image: image.val().trim()
-        });
+        // insertUser({
+        //     name: username.val().trim(),
+        //     password: password.val().trim(),
+        //     image: image.val().trim()
+        // });
+    }
+
+    function checkIfNameTaken(username){
+        $.get("/api/getusers", function(users){
+            for(i = 0; i < users.length; i++){
+                if(username.val() === users[i].name){
+                    swal("Sorry", 'That username is unavailable', 'error');
+                    return;
+                }
+            } insertUser({
+                name: username.val().trim(),
+                password: password.val().trim(),
+                image: image.val().trim()
+            });
+        })
     }
 
     function insertUser(userData) {
@@ -48,11 +64,11 @@ $(document).ready(function () {
         sessionStorage.userId = data.id;
         sessionStorage.username = data.name;
         sessionStorage.image = data.image;
-        let userId = data.id;
-        let userRoute = "/" + userId;
+        let name = data.name;
+        let userRoute = "/" + name;
         swal({
             type: 'success',
-            title: 'Welcome ' + data.name + "!",
+            title: 'Welcome ' + name + "!",
             text: 'Thank you for joining oMe!.',
             timer: 3000,
             onOpen: () => {
